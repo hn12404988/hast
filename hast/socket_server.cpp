66670e658e7fd9c81974210241a7f5ec67a53f77
Willy <hn12404988@hotmail.com>
2017-02-22 22:37:43 +0800

    minor twist

--- hast/socket_server.cpp
+++ hast/socket_server.cpp
@@ -299,7 +299,7 @@ inline void socket_server::echo_back_msg(const short int socket_index, std::stri
 	send(socket_index, msg.c_str(), msg.length(),0);
 }
 
-inline void socket_server::echo_back_error(const short int socket_index, std::string &msg){
+inline void socket_server::echo_back_error(const short int socket_index, std::string msg){
 	if(msg[0]=='[' || msg[0]=='{'){
 		msg = "0{\"Error\":"+msg+"}";
 	}
