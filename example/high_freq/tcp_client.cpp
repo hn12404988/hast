51509d368d097a823df4052dd964daa11eeac74c
Willy <hn12404988@hotmail.com>
2017-03-26 22:42:16 +0800

    Add one more example `high_freq`

---
+++ example/high_freq/tcp_client.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <chrono>
+#include <thread>
+#include <hast/client_core.h>
+
+int main(){
+	std::string msg;
+	client_core client;
+	short int error_flag;
+	client.set_wait_maximum(1); //Maximum time of waiting reply. Set to 1 seconds. (default 2 seconds)
+	std::vector<std::string> location; // The list of server's address.
+	/**
+	 * The list of server's address.
+	 * tcp/ip: ip:port. (Ex: 127.0.0.1:8888)
+	 * unix: path of socket file. (Ex: /tmp/s1.socket)
+	 **/
+	location.push_back("localhost:8888");
+	short int to_s1 {0}; //Address of server is stored in position 0 of vector.
+	client.import_location(&location); //Import the list to client class.
+	while(true){
+		msg = "hi, this is client 1";
+		error_flag = client.fire(to_s1,msg);
+		if(error_flag==0){
+			std::cout << "/****** Client Success ********/" << std::endl;
+			std::cout << msg << std::endl;
+		}
+		else{
+			std::cout << "/****** Client Fail ********/" << std::endl;
+		}
+		std::cout << "/******************************/" << std::endl;
+		std::this_thread::sleep_for(std::chrono::milliseconds(10));
+	}
+	return 0;
+}
---
+++ example/high_freq/tcp_server.cpp
@@ -0,0 +1,28 @@
+#include <hast/tcp_server.h>
+#include <iostream>
+
+tcp_server server; // In global scope, so every thread can use.
+
+auto execute = [&](const short int index){
+	std::string str; //Some variable you want to use during process.
+	while(server.msg_recv(index)==true){
+		std::cout << "/****** Server Success ********/" << std::endl;
+		std::cout << "recv in thread: " << index << std::endl;
+		std::cout << "msg is: " << server.raw_msg[index] << std::endl;
+		std::cout << "/******************************/" << std::endl;
+		server.echo_back_msg(server.socketfd[index],"Got it!");
+	}
+	server.done(index); // Close this thread.
+	return;
+};
+
+int main(){
+	server.execute = execute; //Bind the lambda function to the server.
+	if(server.init("8888")==false){ //Name of this file as the name of socket file. (.cpp will be change to .socket)
+		std::cout << "Server can't init at file: " << __FILE__ << std::endl;
+	}
+	else{
+		server.start_accept();
+	}
+	return 0;
+}
